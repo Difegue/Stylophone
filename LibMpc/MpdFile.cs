750781fc60ff896358ce37f093a24f9d143edcb1
glucaci <gabriel.lucaci.dev@gmail.com>
2016-11-30 12:38:40 +0100

    Copyright headers and using cleaned up.

--- LibMpc/MpdFile.cs
+++ LibMpc/MpdFile.cs
@@ -1,22 +1,4 @@
-﻿/*
- * Copyright 2008 Matthias Sessler
- * 
- * This file is part of LibMpc.net.
- *
- * LibMpc.net is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 2.1 of the License, or
- * (at your option) any later version.
- *
- * LibMpc.net is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with LibMpc.net.  If not, see <http://www.gnu.org/licenses/>.
- */
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 
