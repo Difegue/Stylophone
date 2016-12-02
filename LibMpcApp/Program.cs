c95c09fa2c9918d387997c626ea716eb8a99ac63
glucaci <gabriel.lucaci.dev@gmail.com>
2016-12-02 15:19:18 +0100

    Console app for test created.

---
+++ LibMpcApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using LibMpc;
+
+namespace LibMpcApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var mpc = new Mpc(new IPEndPoint(IPAddress.Loopback, 6600));
+
+            var connected = mpc.ConnectAsync().GetAwaiter().GetResult();
+            if (connected)
+            {
+                StartReadCommands(mpc);
+            }
+            else
+            {
+                Console.WriteLine("Could not connect to MPD");
+            }
+        }
+
+        private static void StartReadCommands(Mpc mpc)
+        {
+            
+        }
+    }
+}
