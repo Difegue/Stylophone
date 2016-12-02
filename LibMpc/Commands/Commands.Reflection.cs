e1430d193f0e493d9c5a953986a39dace1a01a75
glucaci <gabriel.lucaci.dev@gmail.com>
2016-12-02 13:26:01 +0100

    Reflection commands

---
+++ LibMpc/Commands/Commands.Reflection.cs
@@ -0,0 +1,28 @@
+﻿namespace LibMpc
+{
+    public partial class Commands
+    {
+        /// <summary>
+        /// https://www.musicpd.org/doc/protocol/reflection_commands.html
+        /// </summary>
+        public static class Reflection
+        {
+            // TODO: config
+            // TODO: commands
+            // TODO: notcommands
+
+            public class TagTypes : IMpcCommand
+            {
+                public string Value => "tagtypes";
+
+                public object ParseResponse(object response)
+                {
+                    throw new System.NotImplementedException();
+                }
+            }
+
+            // TODO: urlhandlers
+            // TODO: decoders
+        }
+    }
+}
