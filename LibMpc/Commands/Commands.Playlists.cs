16619f7c44cc166407e6a7aea931d7bfb2e5aef8
glucaci <gabriel.lucaci.dev@gmail.com>
2016-12-20 14:54:57 +0100

    Class for Playlist commands added. Test playlists created.

---
+++ LibMpc/Commands/Commands.Playlists.cs
@@ -0,0 +1,24 @@
+﻿namespace LibMpc
+{
+    public partial class Commands
+    {
+        public static class Playlists
+        {
+            /// <summary>
+            /// https://www.musicpd.org/doc/protocol/queue.html
+            /// </summary>
+            public static class Current
+            {
+                
+            }
+
+            /// <summary>
+            /// https://www.musicpd.org/doc/protocol/playlist_files.html
+            /// </summary>
+            public static class Stored
+            {
+                
+            }
+        }
+    }
+}
