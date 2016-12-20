b12ded8121ee6c5360a84b22108f83cf5153b149
glucaci <gabriel.lucaci.dev@gmail.com>
2016-12-20 15:26:09 +0100

    listplaylists command created + test.

--- LibMpc/Commands/Commands.Playlists.cs
+++ LibMpc/Commands/Commands.Playlists.cs
@@ -1,4 +1,8 @@
-﻿namespace LibMpc
+﻿using System.Collections.Generic;
+using System.Linq;
+using LibMpc.Types;
+
+namespace LibMpc
 {
     public partial class Commands
     {
@@ -17,7 +21,32 @@
             /// </summary>
             public static class Stored
             {
-                
+                /// <summary>
+                /// Prints a list of the playlist directory.
+                /// </summary>
+                public class ListPlaylists : IMpcCommand<IEnumerable<MpdPlaylist>>
+                {
+                    public string Value => "listplaylists";
+
+                    public IEnumerable<MpdPlaylist> FormatResponse(IList<KeyValuePair<string, string>> response)
+                    {
+                        var result = new List<MpdPlaylist>();
+
+                        foreach (var line in response)
+                        {
+                            if (line.Key.Equals("playlist"))
+                            {
+                                result.Add(new MpdPlaylist(line.Value));
+                            }
+                            else if (line.Key.Equals("Last-Modified"))
+                            {
+                                result.Last().AddLastModified(line.Value);
+                            }
+                        }
+
+                        return result;
+                    }
+                }
             }
         }
     }
---
+++ LibMpcTest/Tests/PlaylistsCommandsTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Xunit;
+using LibMpc;
+using System.Linq;
+
+namespace LibMpcTest
+{
+    public partial class LibMpcTest
+    {
+        [Fact]
+        public async Task ListPlaylistsTest()
+        {
+            var response = await Mpc.SendAsync(new Commands.Playlists.Stored.ListPlaylists());
+
+            TestOutput.WriteLine($"ListPlaylistsTest Result:");
+            TestOutput.WriteLine(JsonConvert.SerializeObject(response, Formatting.Indented));
+
+            Assert.True(response.Response.Body.Count().Equals(3));
+        }
+    }
+}
