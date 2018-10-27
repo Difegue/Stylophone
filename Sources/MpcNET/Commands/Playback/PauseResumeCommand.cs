19f0f92747d1621d037d1f4a16c57aeec5a0fea7
Kim Hugener-Ohlsen <kho@hukano.com>
2018-10-27 14:47:40 +0200

    Fixed pause command

--- Sources/MpcNET/Commands/Playback/PauseResumeCommand.cs
+++ Sources/MpcNET/Commands/Playback/PauseResumeCommand.cs
@@ -1,5 +1,5 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="PlayPauseCommand.cs" company="MpcNET">
+// <copyright file="PauseResumeCommand.cs" company="MpcNET">
 // Copyright (c) MpcNET. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
@@ -9,18 +9,44 @@ namespace MpcNET.Commands.Playback
     using System.Collections.Generic;
 
     /// <summary>
-    /// Command to play or pause.
+    /// Command to pause or resume.
     /// https://www.musicpd.org/doc/protocol/playback_commands.html.
     /// </summary>
-    public class PlayPauseCommand : IMpcCommand<string>
+    public class PauseResumeCommand : IMpcCommand<string>
     {
+        private readonly string playArgument;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PauseResumeCommand" /> class.
+        /// </summary>
+        /// <param name="pause">if set to <c>true</c> [pause].</param>
+        public PauseResumeCommand(bool pause)
+        {
+            this.playArgument = pause ? "1" : "0";
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PauseResumeCommand"/> class.
+        /// </summary>
+        public PauseResumeCommand()
+        {
+        }
+
         /// <summary>
         /// Serializes the command.
         /// </summary>
         /// <returns>
         /// The serialize command.
         /// </returns>
-        public string Serialize() => string.Join(" ", "pause");
+        public string Serialize()
+        {
+            if (this.playArgument == null)
+            {
+                return string.Join(" ", "pause");
+            }
+
+            return string.Join(" ", "pause", this.playArgument);
+        }
 
         /// <summary>
         /// Deserializes the specified response text pairs.
