c1ade86dc9e2544df78f5ff25809ff13d921b55f
Difegue <sugoi@cock.li>
2020-09-16 22:31:00 +0200

    Remove unused WTS service

--- Sources/FluentMPC/Services/ActivationService.cs
+++ Sources/FluentMPC/Services/ActivationService.cs
@@ -69,7 +69,7 @@ namespace FluentMPC.Services
             await ThemeSelectorService.InitializeAsync().ConfigureAwait(false);
 
             await Singleton<SettingsViewModel>.Instance.EnsureInstanceInitializedAsync(); // This also initializes MPDConnectionService
-            await WindowManagerService.Current.InitializeAsync();
+
         }
 
         private async Task HandleActivationAsync(object activationArgs)
