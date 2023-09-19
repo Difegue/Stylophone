4af8691ae1e2da0b68e46a3d500f1eca25d98dac
Difegue <sugoi@cock.li>
2023-09-19 02:42:15 +0200

    janky inapp notification popover

--- Sources/Stylophone.iOS/Services/NotificationService.cs
+++ Sources/Stylophone.iOS/Services/NotificationService.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using UserNotifications;
 using Stylophone.Common.Interfaces;
-using UIKit;
 
 namespace Stylophone.iOS.Services
 {
     public class NotificationService : NotificationServiceBase
     {
         private IDispatcherService _dispatcherService;
+        private Timer _notificationTimer;
 
         public NotificationService(IDispatcherService dispatcherService)
         {
@@ -16,18 +16,30 @@ namespace Stylophone.iOS.Services
 
         public override void ShowInAppNotification(InAppNotification notification)
         {
+            if (notification.NotificationType == NotificationType.Error)
+            {
+                // Let's just use alerts until TipKit is available... This is cheap but w/e
+                var alert = new UIAlertView(notification.NotificationTitle, notification.NotificationText, null, "Ok");
+                alert.Show();
+                return;
+            }
+
+            var rootVc = (UIApplication.SharedApplication.Delegate as AppDelegate).RootViewController;
+
             UIApplication.SharedApplication.InvokeOnMainThread(() =>
             {
                 if (UIApplication.SharedApplication.ApplicationState != UIApplicationState.Active)
                     return;
 
-                // Let's just use alerts until TipKit is available... This is cheap but w/e
-                var alert = new UIAlertView(notification.NotificationTitle, notification.NotificationText, null, "Ok");
+                var popover = new NotificationPopoverViewController(notification.NotificationTitle, rootVc);
 
-                if (notification.NotificationType == NotificationType.Error) 
-                    alert.Show();
-
+                rootVc.PresentViewController(popover, true, null);
             });
+
+            _notificationTimer?.Dispose();
+            _notificationTimer = new Timer((_) => UIApplication.SharedApplication.InvokeOnMainThread(() =>
+                        rootVc.DismissViewController(true, null)),
+                    null, 1500, Timeout.Infinite);
         }
 
         public override void ShowBasicToastNotification(string title, string description)
@@ -46,4 +58,48 @@ namespace Stylophone.iOS.Services
             });
         }
     }
+
+    public class NotificationPopoverViewController : UIViewController, IUIPopoverPresentationControllerDelegate
+    {
+        private string _text;
+
+        public NotificationPopoverViewController(string text, UISplitViewController rootVc) //, CGRect sourceBounds, CGSize contentSize)
+        {
+            _text = text;
+
+            ModalPresentationStyle = UIModalPresentationStyle.Popover;
+
+            PopoverPresentationController.SourceView = rootVc.View;
+            PopoverPresentationController.Delegate = this;
+            PopoverPresentationController.PermittedArrowDirections = UIPopoverArrowDirection.Left;
+            PopoverPresentationController.SourceRect = new CGRect(16, 64, 1, 1);
+        }
+
+        public override void LoadView()
+        {
+            base.LoadView();
+
+            PreferredContentSize = new CGSize(196, 54);
+
+            View = new UILabel
+            {
+                Text = _text,
+                Font = UIFont.PreferredHeadline,
+                TextAlignment = UITextAlignment.Center,
+                AdjustsFontSizeToFitWidth = true,
+                Lines = 2,
+            };
+
+        }
+
+        [Export("adaptivePresentationStyleForPresentationController:traitCollection:")]
+        public UIModalPresentationStyle GetAdaptivePresentationStyle(UIPresentationController controller, UITraitCollection traitCollection)
+        {
+            // Prevent popover from being adaptive fullscreen on phones
+            // (https://pspdfkit.com/blog/2022/presenting-popovers-on-iphone-with-swiftui/)
+            return UIModalPresentationStyle.None;
+        }
+
+    }
+
 }
