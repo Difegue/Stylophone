808c3f09c42d3d3ef3d2a630be6f48af839fb882
Difegue <8237712+Difegue@users.noreply.github.com>
2024-08-13 00:37:13 +0200

    Add client widgetopia classes

---
+++ Sources/Stylophone/PayloadSender.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Windows.ApplicationModel.AppService;
+using Windows.Storage;
+using Windows.Foundation.Collections;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Widgetopia.Core;
+public static class PayloadSender
+{
+    public const string APPSERVICE_NAME = "com.widgetopia.service";
+    public const string PACKAGE_FAMILYNAME = "047166cb-9231-4829-8c73-3227576cbf91_sm6ssk3qw71mj";
+    public static async Task<ValueSet> SendDataAsync(WidgetPayload payload)
+    {
+        var message = new ValueSet();
+        var result = new ValueSet();
+        var connection = new AppServiceConnection()
+        {
+            // Here, we use the app service name defined in the app service 
+            // provider's Package.appxmanifest file in the <Extension> section.
+            AppServiceName = APPSERVICE_NAME,
+            // Use Windows.ApplicationModel.Package.Current.Id.FamilyName 
+            // within the app service provider to get this value.
+            PackageFamilyName = PACKAGE_FAMILYNAME
+        };
+
+        var status = await connection.OpenAsync();
+
+        if (status != AppServiceConnectionStatus.Success)
+        {
+            result.Add("Status", false);
+            result.Add("Error", "Failed to connect");
+            return result;
+        }
+
+        try
+        {
+            // Serialize payload and send it to service
+            message.Add("WidgetPayload", JsonSerializer.Serialize(payload));
+            AppServiceResponse response = await connection.SendMessageAsync(message);
+
+            if (response.Status == AppServiceResponseStatus.Success)
+            {
+                // Get the data that the service sent to us.
+                return response.Message;
+            }
+            else
+            {
+                result.Add("Status", false);
+                result.Add("Error", "Incorrect response received from app service (" + response.Status + ")");
+            }
+        }
+        catch (Exception e)
+        {
+            result.Add("Status", false);
+            result.Add("Error", "Error while communicating with app service");
+            result.Add("Exception", e);
+        }
+
+        return result;
+
+    }
+
+    public static string ReadPackageFileFromUri(string packageUri)
+    {
+        try
+        {
+            var uri = new Uri(packageUri);
+            var storageFileTask = StorageFile.GetFileFromApplicationUriAsync(uri).AsTask();
+            storageFileTask.Wait();
+
+            var readTextTask = FileIO.ReadTextAsync(storageFileTask.Result).AsTask();
+            readTextTask.Wait();
+
+            return readTextTask.Result;
+        }
+        catch (Exception e)
+        {
+            return e.ToString();
+        }
+        
+    }
+
+}
---
+++ Sources/Stylophone/WidgetPayload.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.Json;
+
+namespace Widgetopia.Core;
+
+public record WidgetPayload
+{
+    /// <summary>
+    /// Unique ID for the App sending the payload.
+    /// Make sure to preface your data keys with this ID.
+    /// e.g, with an ID of "mycoolapp", "mycoolapp_mydata".
+    /// </summary>
+    public string Id { get; init; }
+
+    /// <summary>
+    /// Friendly name for the App to show on the UI
+    /// </summary>
+    public string AppName { get; init; }
+
+    /// <summary>
+    /// Base64-encoded icon for the App
+    /// </summary>
+    public string? Base64Icon { get; init; }
+
+    /// <summary>
+    /// Prebuilt Widget Templates for the App
+    /// </summary>
+    public string[]? Templates { get; init; }
+
+    /// <summary>
+    /// Up-to-date data you want to show in widgets.
+    /// Make sure to prepend your data keys with the app ID, eg "mycoolapp_mydata".
+    /// </summary>
+    public WidgetData[] Data { get; init; }
+
+    /// <summary>
+    /// A URL Widgetopia should invoke when widget actions are executed.
+    /// If the action has a verb, it'll be appended to the end of the URL.
+    /// </summary>
+    public string ActionCallbackUrl { get; init; }
+}
+
+public enum DataType
+{
+    STRING,
+    BOOL,
+    INT,
+    FLOAT,
+    IMAGE // URL or path to an image. Widgetopia doesn't cache images and will pass this directly to the widget renderer.
+}
+
+public record WidgetData
+{
+    public DataType Type { get; init; }
+    public string Key { get; init; }
+    public string Value { get; init; }
+}
