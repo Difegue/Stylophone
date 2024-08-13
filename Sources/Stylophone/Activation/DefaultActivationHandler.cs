2984e76140276fb44c91c47d2f71c624c95e76fc
Difegue <8237712+Difegue@users.noreply.github.com>
2024-08-13 23:09:06 +0200

    Add protocol activation handler

--- Sources/Stylophone/Activation/DefaultActivationHandler.cs
+++ Sources/Stylophone/Activation/DefaultActivationHandler.cs
@@ -3,6 +3,12 @@ using System.Threading.Tasks;
 using Stylophone.Services;
 using Stylophone.Common.Interfaces;
 using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml;
+using CommunityToolkit.Mvvm.DependencyInjection;
+using Stylophone.Common.Services;
+using Stylophone.Common.ViewModels;
+using System.Threading;
 
 namespace Stylophone.Activation
 {
@@ -29,13 +35,49 @@ namespace Stylophone.Activation
             }
 
             _navigationService.Navigate(_navElement, arguments);
-            await Task.CompletedTask;
+
+            // Ensure the current window is active
+            Window.Current.Activate();
+
+            // Tasks after activation
+            await StartupAsync();
+        }
+
+        private async Task StartupAsync()
+        {
+            var theme = Ioc.Default.GetRequiredService<IApplicationStorageService>().GetValue<string>(nameof(SettingsViewModel.ElementTheme));
+            Enum.TryParse(theme, out Theme elementTheme);
+            await Ioc.Default.GetRequiredService<IInteropService>().SetThemeAsync(elementTheme);
+
+            await Ioc.Default.GetRequiredService<IDialogService>().ShowFirstRunDialogIfAppropriateAsync();
+
+            _ = Task.Run(async () =>
+            {
+                Thread.Sleep(60000);
+                await Ioc.Default.GetRequiredService<IDialogService>().ShowRateAppDialogIfAppropriateAsync();
+            });
+
+            var host = Ioc.Default.GetRequiredService<IApplicationStorageService>().GetValue<string>(nameof(SettingsViewModel.ServerHost));
+            host = host?.Replace("\"", ""); // TODO: This is a quickfix for 1.x updates
+            var port = Ioc.Default.GetRequiredService<IApplicationStorageService>().GetValue<int>(nameof(SettingsViewModel.ServerPort), 6600);
+            var pass = Ioc.Default.GetRequiredService<IApplicationStorageService>().GetValue<string>(nameof(SettingsViewModel.ServerPassword));
+            var localPlaybackEnabled = Ioc.Default.GetRequiredService<IApplicationStorageService>().GetValue<bool>(nameof(SettingsViewModel.IsLocalPlaybackEnabled));
+
+            var localPlaybackVm = Ioc.Default.GetRequiredService<LocalPlaybackViewModel>();
+            localPlaybackVm.Initialize(host, localPlaybackEnabled);
+
+            var mpdService = Ioc.Default.GetRequiredService<MPDConnectionService>();
+            mpdService.SetServerInfo(host, port, pass);
+            await mpdService.InitializeAsync(true);
+
+            Ioc.Default.GetRequiredService<AlbumArtService>().Initialize();
+            Ioc.Default.GetRequiredService<SystemMediaControlsService>().Initialize();
         }
 
         protected override bool CanHandleInternal(IActivatedEventArgs args)
         {
             // None of the ActivationHandlers has handled the app activation
-            return ((NavigationService)_navigationService).Frame.Content == null && _navElement != null;
+            return ((NavigationService)_navigationService).Frame?.Content == null && _navElement != null;
         }
     }
 }
---
+++ Sources/Stylophone/Activation/ProtocolActivationHandler.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Threading.Tasks;
+using Stylophone.Services;
+using Stylophone.Common.Interfaces;
+using Windows.ApplicationModel.Activation;
+using CommunityToolkit.Mvvm.DependencyInjection;
+using Stylophone.Common.Services;
+using Windows.UI.Xaml.Controls;
+using MpcNET.Commands.Playback;
+using MpcNET.Commands.Status;
+using MpcNET.Commands.Playlist;
+using System.Xml.Linq;
+using Stylophone.Common.ViewModels;
+using Windows.Services.Maps;
+
+namespace Stylophone.Activation
+{
+    internal class ProtocolActivationHandler : ActivationHandler<IActivatedEventArgs>
+    {
+        private MPDConnectionService _mpdService;
+
+        public ProtocolActivationHandler(MPDConnectionService connectionService)
+        {
+            _mpdService = connectionService;
+        }
+
+        protected override async Task HandleInternalAsync(IActivatedEventArgs args)
+        {
+            ProtocolActivatedEventArgs eventArgs = args as ProtocolActivatedEventArgs;
+
+            if (!_mpdService.IsConnected)
+            {
+                var host = Ioc.Default.GetRequiredService<IApplicationStorageService>().GetValue<string>(nameof(SettingsViewModel.ServerHost));
+                host = host?.Replace("\"", ""); // TODO: This is a quickfix for 1.x updates
+                var port = Ioc.Default.GetRequiredService<IApplicationStorageService>().GetValue<int>(nameof(SettingsViewModel.ServerPort), 6600);
+                var pass = Ioc.Default.GetRequiredService<IApplicationStorageService>().GetValue<string>(nameof(SettingsViewModel.ServerPassword));
+                _mpdService.SetServerInfo(host, port, pass);
+                await _mpdService.InitializeAsync(false);
+            }
+
+            if (!_mpdService.IsConnected)
+            {
+                var msgbox = new ContentDialog
+                {
+                    Title = "Couldn't connect to MPD server",
+                    Content = "Please open Stylophone and configure a MPD server.",
+                    CloseButtonText = "OK"
+                };
+                await msgbox.ShowAsync();
+                return;
+            }
+
+            var status = _mpdService.CurrentStatus == MPDConnectionService.BOGUS_STATUS ? await _mpdService.SafelySendCommandAsync(new StatusCommand()) : _mpdService.CurrentStatus;
+
+            // Protocol launches can do basic operations on the MPD server based on the verb
+            // eg stylophone://?verb=stylophone_play
+            var uri = new Uri(eventArgs.Uri.AbsoluteUri);
+            var queryDictionary = System.Web.HttpUtility.ParseQueryString(uri.Query);
+
+            switch (queryDictionary["verb"])
+            {
+                case "stylophone_play":
+                case "stylophone_pause":
+                    await _mpdService.SafelySendCommandAsync(new PauseResumeCommand());
+                    break;
+                case "stylophone_stop":
+                    await _mpdService.SafelySendCommandAsync(new StopCommand());
+                    break;
+                case "stylophone_next":
+                    await _mpdService.SafelySendCommandAsync(new NextCommand());
+                    break;
+                case "stylophone_prev":
+                    await _mpdService.SafelySendCommandAsync(new PreviousCommand());
+                    break;
+                case "stylophone_shuffle":
+                    await _mpdService.SafelySendCommandAsync(new RandomCommand(!status.Random));
+                    break;
+                case "stylophone_volume_up":
+                    await _mpdService.SafelySendCommandAsync(new SetVolumeCommand((byte)(status.Volume + 5)));
+                    break;
+                case "stylophone_volume_down":
+                    await _mpdService.SafelySendCommandAsync(new SetVolumeCommand((byte)(status.Volume - 5)));
+                    break;
+                case "stylophone_volume_set":
+                    var volume = queryDictionary["volume"] ?? "0";
+                    await _mpdService.SafelySendCommandAsync(new SetVolumeCommand((byte)(int.Parse(volume))));
+                    break;
+                case "stylophone_seek":
+                    var seek = int.Parse(queryDictionary["volseekume"] ?? "0");
+                    await _mpdService.SafelySendCommandAsync(new SeekCurCommand(seek));
+                    break;
+                case "stylophone_load_playlist":
+                    var playlist = queryDictionary["playlist"] ?? "";
+                    await _mpdService.SafelySendCommandAsync(new LoadCommand(playlist));
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        protected override bool CanHandleInternal(IActivatedEventArgs args)
+        {
+            return args.Kind == ActivationKind.Protocol;
+        }
+    }
+}
